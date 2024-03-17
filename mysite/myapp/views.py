5b994d665a79506bfddf3a9a1bd8d58798249211
AqiuNoCold <1213758223@qq.com>
2024-03-17 16:22:54 +0800

    3.17 For-Study

--- mysite/myapp/views.py
+++ mysite/myapp/views.py
@@ -3,8 +3,8 @@ from django.http import HttpResponse
 from myapp.models import SpiderDb
 # Create your views here.
 def user_list(request):
-    Topic = SpiderDb.objects.all()
-    for obj in Topic:
+    topic = SpiderDb.objects.all()
+    for obj in topic:
         print(obj.origin,obj.author)
     print(request.method)
     print(request.GET)
@@ -14,7 +14,7 @@ def user_list(request):
     # 可以直接选择抓包获取数据！！！
     # import requests
     # r = requests.get('https://jsonplaceholder.typicode.com/posts/1')
-    #return redirect("https://www.baidu.com")
+    # return redirect("https://www.baidu.com")
     return render(request,'user_list.html',{"n1":name,"n2":roles,"n3":user_info})
 
 def login(request):
@@ -25,4 +25,7 @@ def login(request):
         else:
             return render(request,'login.html',{"error_msg":"用户名或密码错误"})
     if request.method == "GET":
-       return render(request,'login.html')
+        return render(request,'login.html')
+
+def demo(request):
+    return HttpResponse("Demo_01")
--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -20,5 +20,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('user/list',views.user_list),
     path('login',views.login),
+    path('demo',views.demo)
 ]
 
